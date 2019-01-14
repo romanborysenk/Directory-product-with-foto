68a935064283ee16683b4e37b1a5aa2e201226f3
root <root@localhost.localdomain>
2019-01-14 16:09:47 +0200

    create groups part 1

--- foto-download/src/main/java/project/kuper/fotodownload/application/controllers/GroupScreenController.java
+++ foto-download/src/main/java/project/kuper/fotodownload/application/controllers/GroupScreenController.java
@@ -2,15 +2,36 @@ package project.kuper.fotodownload.application.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import project.kuper.fotodownload.application.model.Group;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/main/group")
 public class GroupScreenController {
+    private List<Group> list;
 
     @GetMapping
     public String getGroupScreen(Model model) {
-        return"group";
+        list = new LinkedList<>();
+        list.add(new Group(1,"1"));
+        list.add(new Group(2,"2"));
+        list.add(new Group(3,"3"));
+        list.add(new Group(4,"4"));
+        list.add(new Group(5,"5"));
+        model.addAttribute("list",list);
+        return "group";
+    }
+
+    @RequestMapping(params="insert", method = RequestMethod.POST)
+    public String addItemListGroup(@RequestParam("nameGroup") String nameGroup, Model model) {
+        Integer id = list.size();
+        id++;
+        list.add(new Group(id,nameGroup));
+        model.addAttribute("list",list);
+        return "group";
     }
 }
--- foto-download/src/main/java/project/kuper/fotodownload/application/model/Group.java
+++ foto-download/src/main/java/project/kuper/fotodownload/application/model/Group.java
@@ -2,13 +2,28 @@ package project.kuper.fotodownload.application.model;
 
 import lombok.Data;
 
-@Data
 public class Group {
     private Integer id;
     private String name;
 
     public Group(Integer id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
